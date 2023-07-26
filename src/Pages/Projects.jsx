d66a8fb8d67f3949508385c1212d458ebed2c673
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2023-07-26 21:51:10 +0200

    suppression d'un console.log

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,7 +4,6 @@ import projectsList from "../constants/ProjectsList";
 
 const Projects = () => {
   const [orderedProjects] = useState(projectsList.reverse());
-  console.log(orderedProjects);
   return (
     <>
       <div className="lg:ml-[175px] my-40 md:my-20 mx-16 2xl:p-0 flex flex-col md:flex-row md:flex-wrap md:justify-center">
