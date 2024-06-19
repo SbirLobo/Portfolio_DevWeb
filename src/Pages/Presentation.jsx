7c2053dfc355edb002d4fe44598240d54f6cd0bb
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2024-06-19 21:07:01 +0200

    update CV

--- src/Pages/Presentation.jsx
+++ src/Pages/Presentation.jsx
@@ -4,20 +4,20 @@ const Presentation = () => {
   return (
     <div className="lg:ml-[175px] my-40 md:my-20 flexV">
       <p className="mb-4 px-4 text-center text-xl">
-        Bonjour et merci d'être ici avec moi,
+        Bonjour et merci d&#39;être ici avec moi,
       </p>
       <Intro />
       <div className="flex flex-col sm:flex-row justify-between items-center w-full pb-8">
         <img
-          className="w-44 p-4"
-          src="https://github.com/SbirLobo/Portfolio_DevWeb/assets/108263661/858c3a75-130c-4d7e-9ec8-6449936e3d12"
+          className="w-44 p-4 rounded-full border-2"
+          src="https://github.com/SbirLobo/Portfolio_DevWeb/assets/108263661/a60db72b-ac36-40c9-a353-8e339ff88041"
           alt="photo sbirlobo"
         />
         <div className="flexV">
           <a
             rel="noreferrer"
             target="_blank"
-            href="https://github.com/user-attachments/files/15904805/CV_s.birolleau_devWeb_alt.pdf"
+            href="https://github.com/user-attachments/files/15904952/CV_s.birolleau_devWeb_alt2.pdf"
           >
             <img
               src="https://github.com/SbirLobo/Portfolio_DevWeb/assets/108263661/e639dda9-e073-4038-aa8f-ed2cf92ce478"
@@ -27,7 +27,7 @@ const Presentation = () => {
           <p className="px-2 italic text-sm">mon CV</p>
         </div>
       </div>
-      <p className="mb-2">Ah, j'oubliais... J'adore l'algo !</p>
+      <p className="mb-2">Ah, j&#39;oubliais... J&#39;adore l&#39;algo !</p>
       <img
         src="https://www.codewars.com/users/sbir51/badges/large"
         alt="badge code wars"
